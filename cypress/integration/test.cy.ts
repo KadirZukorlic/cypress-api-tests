94d667f651d30523be27f7dfcec27bfc5a7d8be4
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-11-11 02:08:04 +0100

    Test global feed likes count

--- cypress/integration/test.cy.ts
+++ cypress/integration/test.cy.ts
@@ -35,4 +35,30 @@ describe("Test with backend", () => {
       .and("contain", "automation")
       .and("contain", "testing");
   });
+
+  it.only("Verify global feed likes count", () => {
+    cy.intercept("GET", "https://api.realworld.io/api/articles/feed*", {
+      articles: [],
+      articlesCount: 0,
+    });
+    cy.intercept("GET", "https://api.realworld.io/api/articles*", {
+      fixture: "articles.json",
+    });
+    cy.contains("Global Feed").click();
+    cy.get("app-article-list button").then((heartList) => {
+      expect(heartList[0]).to.contain("1");
+      expect(heartList[1]).to.contain("5");
+    });
+
+    cy.fixture("articles").then((file) => {
+      const articleLink = file.articles[1].slug;
+      file.articles[1].favoritesCount = 6;
+      cy.intercept(
+        "POST",
+        // "https://api.realworld.io/api/articles/"+articleLink+"/favorite"
+        `https://api.realworld.io/api/articles/${articleLink}/favorite`
+      );
+    });
+    cy.get("app-article-list button").eq(1).click().should("contain", "6");
+  });
 });
