332adb2481dccea6d0adbf3f8571f9bf8790eb7a
Kadir Zukorlic <kadirzukorlic@gmail.com>
2022-11-15 01:17:17 +0100

    Add method and modify request with intercept

--- cypress/integration/test.cy.ts
+++ cypress/integration/test.cy.ts
@@ -1,8 +1,11 @@
 describe("Test with backend", () => {
   beforeEach("login to application", () => {
-    cy.intercept("GET", "https://api.realworld.io/api/tags", {
-      fixture: "tags.json",
-    });
+    cy.intercept(
+      { method: "Get", path: "tags" },
+      {
+        fixture: "tags.json",
+      }
+    );
     cy.loginToApplication();
   });
 
@@ -29,14 +32,44 @@ describe("Test with backend", () => {
     });
   });
 
-  it("Verify popular tags that are displayed", () => {
+  it("Intercepting and modifying the request and respone", () => {
+    // cy.intercept("POST", "https://api.realworld.io/api/articles/", (req) => {
+    //   req.body.article.description = "This is a description 2";
+    // }).as("postArticles");
+
+    cy.intercept("POST", "https://api.realworld.io/api/articles/", (req) => {
+      req.reply((res) => {
+        expect(res.body.article.description).to.equal("This is a description");
+        res.body.article.description = "This is a description 2";
+      });
+    }).as("postArticles");
+
+    cy.contains("New Article").click();
+    cy.get('[formcontrolname="title"]').type("The normal Title");
+    cy.get('[formcontrolname="description"]').type("This is a description");
+    cy.get('[formcontrolname="body"]').type("This is a body of the article");
+    cy.contains("Publish Article").click();
+
+    cy.wait("@postArticles").then((xhr) => {
+      console.log(xhr);
+      expect(xhr.response.statusCode).to.equal(200);
+      expect(xhr.request.body.article.body).to.equal(
+        "This is a body of the article"
+      );
+      expect(xhr.response.body.article.description).to.equal(
+        "This is a description 2"
+      );
+    });
+  });
+
+  it.only("Verify popular tags that are displayed", () => {
     cy.get(".tag-list")
       .should("contain", "cypress")
       .and("contain", "automation")
       .and("contain", "testing");
   });
 
-  it.only("Verify global feed likes count", () => {
+  it("Verify global feed likes count", () => {
     cy.intercept("GET", "https://api.realworld.io/api/articles/feed*", {
       articles: [],
       articlesCount: 0,
